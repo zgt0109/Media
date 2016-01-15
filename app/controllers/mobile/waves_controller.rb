b812ea87cdace02878ec6a9fea68f63d18b5c0fb
Mike <wenke.gd@gmail.com>
2016-01-15 18:59:25 +0800

    fix wave

--- app/controllers/mobile/waves_controller.rb
+++ app/controllers/mobile/waves_controller.rb
@@ -18,11 +18,11 @@ class Mobile::WavesController < Mobile::BaseController
   end
 
   def check
-    wx_user_lottery_draws = @activity.lottery_draws.where(user_id: session[:user_id])
-    if (@activity.activity_property.day_partake_limit == -1) || (wx_user_lottery_draws.today.count < @activity.activity_property.day_partake_limit) #每人每天参与次数
-      if (@activity.activity_property.partake_limit == -1) || (wx_user_lottery_draws.count < @activity.activity_property.partake_limit)#每人参与总次数
-        if @activity.activity_property.prize_limit == -1 or wx_user_lottery_draws.win.count < @activity.activity_property.prize_limit #每人总中奖次数
-          if @activity.activity_property.day_prize_limit == -1 or wx_user_lottery_draws.today.win.count < @activity.activity_property.day_prize_limit #每人每天中奖次数
+    user_lottery_draws = @activity.lottery_draws.where(user_id: session[:user_id])
+    if (@activity.activity_property.day_partake_limit == -1) || (user_lottery_draws.today.count < @activity.activity_property.day_partake_limit) #每人每天参与次数
+      if (@activity.activity_property.partake_limit == -1) || (user_lottery_draws.count < @activity.activity_property.partake_limit)#每人参与总次数
+        if @activity.activity_property.prize_limit == -1 or user_lottery_draws.win.count < @activity.activity_property.prize_limit #每人总中奖次数
+          if @activity.activity_property.day_prize_limit == -1 or user_lottery_draws.today.win.count < @activity.activity_property.day_prize_limit #每人每天中奖次数
           else
             return render json: { error_msg: "明天再来" }
           end
@@ -44,17 +44,17 @@ class Mobile::WavesController < Mobile::BaseController
       @prize_title = '谢谢参与'
       logger.info "========活动进行中"
       lottery_draws         = @activity.lottery_draws
-      wx_user_lottery_draws = lottery_draws.where(user_id: user_id)
-      if @activity.activity_property.day_partake_limit == -1 or wx_user_lottery_draws.today.count < @activity.activity_property.day_partake_limit #每人每天参与次数
-        if @activity.activity_property.partake_limit == -1 or wx_user_lottery_draws.count < @activity.activity_property.partake_limit #每人参与总次数
-          if @activity.activity_property.day_prize_limit == -1 or wx_user_lottery_draws.today.win.count < @activity.activity_property.day_prize_limit #每人每天中奖次数
-            if @activity.activity_property.prize_limit == -1 or wx_user_lottery_draws.win.count < @activity.activity_property.prize_limit #每人总中奖次数
+      user_lottery_draws = lottery_draws.where(user_id: user_id)
+      if @activity.activity_property.day_partake_limit == -1 or user_lottery_draws.today.count < @activity.activity_property.day_partake_limit #每人每天参与次数
+        if @activity.activity_property.partake_limit == -1 or user_lottery_draws.count < @activity.activity_property.partake_limit #每人参与总次数
+          if @activity.activity_property.day_prize_limit == -1 or user_lottery_draws.today.win.count < @activity.activity_property.day_prize_limit #每人每天中奖次数
+            if @activity.activity_property.prize_limit == -1 or user_lottery_draws.win.count < @activity.activity_property.prize_limit #每人总中奖次数
               lottery = @activity.lottery_draws.create(site_id: @activity.site_id, user_id: user_id, status: 0)
               prize   = @activity.get_prize #获取奖品
               if prize #如果中奖.where(user_id: user_id
                 logger.info "========获取到奖券==========="
-                if prize.people_day_limit_count == -1 or wx_user_lottery_draws.where(activity_prize_id: prize.id).today.count < prize.people_day_limit_count # 某奖品每人每天次数
-                  if prize.people_limit_count == -1 or wx_user_lottery_draws.where(activity_prize_id: prize.id).count < prize.people_limit_count # 某奖品每人总次数
+                if prize.people_day_limit_count == -1 or user_lottery_draws.where(activity_prize_id: prize.id).today.count < prize.people_day_limit_count # 某奖品每人每天次数
+                  if prize.people_limit_count == -1 or user_lottery_draws.where(activity_prize_id: prize.id).count < prize.people_limit_count # 某奖品每人总次数
                     if prize.day_limit_count == -1 or lottery_draws.where(activity_prize_id: prize.id).today.count < prize.day_limit_count # 某奖品当天次数
                       if lottery_draws.where(activity_prize_id: prize.id).count < prize.prize_count # 奖品数量 如果奖池中还有奖品
                         logger.info "========生成奖品记录,奖品为#{prize.title}============="
@@ -106,7 +106,7 @@ class Mobile::WavesController < Mobile::BaseController
   end
 
   def prizes
-    @activity_consumes = @wx_user.present? ? @wx_user.activity_consumes.includes(:activity_prize).where(activity_id: @activity.id).order(:created_at) : []
+    @activity_consumes = @user.present? ? @user.activity_consumes.includes(:activity_prize).where(activity_id: @activity.id).order(:created_at) : []
   end
 
   def draw_prize
