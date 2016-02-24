fbe76286102209dcc3ecf60ca0baa30c2948378a
icepoint0 <351711778@qq.com>
2016-02-24 15:04:52 +0800

    wx_groups

---
+++ app/controllers/wx/messages_controller.rb
@@ -0,0 +1,20 @@
+class Wx::MessagesController <  ApplicationController
+
+  before_filter do
+    @partialLeftNav = "/layouts/partialLeftWeixin"
+  end
+
+  def index
+
+  end
+
+  def send_message
+    options = {:media_type=>"text",:is_to_all=>true ,:content=>params[:text_message]}
+    notice=  current_site.wx_mp_user.message_mass_send_all(options)["msg_id"].nil? ? '保存失败消息发送超出上限' : '保存成功'
+
+    respond_to do |format|
+      format.html { redirect_to :back, notice: notice }
+    end
+    
+  end
+end
--- app/models/concerns/weixin_api.rb
+++ app/models/concerns/weixin_api.rb
@@ -1,12 +1,10 @@
 module Concerns::WeixinApi
 
-# 获取用户列表
-  def wx_user_list
-    url = "https://api.weixin.qq.com/cgi-bin/user/get?access_token=#{self.wx_access_token}&next_openid="
+  def wx_user_list( options={} )
+    url = "https://api.weixin.qq.com/cgi-bin/user/get?access_token=#{self.wx_access_token}&next_openid=#{options[:next_openid]}"
     get_request(url)["data"]["openid"]
   end
 
-# 获取用户基本信息
   def wx_user_info
     wx_user_list.each do |l|
       @wxuser=WxUser.new(wx_mp_user_id: self.id)
@@ -16,66 +14,139 @@ module Concerns::WeixinApi
       @wxuser.save
     end
   end
-# 查询所有分组
+
   def wx_user_group_list
     #code
     url = "https://api.weixin.qq.com/cgi-bin/groups/get?access_token=#{self.wx_access_token}"
     groups = get_request(url)["groups"]
-    # groups.each do |g|
-    #   group = WxUserGroup.where(groupid:g["id"]).first_or_create
-    #   group.update_attributes(name: g["name"],count:g["count"],wx_mp_user_id:self.id,groupid:g["id"])
-    # end
   end
-# 创建分组
-  def create_user_group name
+
+  def create_user_group( name )
     url = "https://api.weixin.qq.com/cgi-bin/groups/create?access_token=#{self.wx_access_token}"
     parameters = {:group => {:name => name}}.to_json
     post_request(url,parameters)
   end
-  # 修改分组名
-  def change_group_name options={}
 
+  def change_group_name( options={} )
     parameters = {:group =>{:id =>options[:id],:name =>options[:name]}}.to_json
     url = "https://api.weixin.qq.com/cgi-bin/groups/update?access_token=#{self.wx_access_token}"
     post_request(url,parameters)
 
   end
-  # 删除分组
-  def delete_wx_group id
+
+  def delete_wx_group( id )
     url="https://api.weixin.qq.com/cgi-bin/groups/delete?access_token=#{self.wx_access_token}"
     parameters = {:group=>{:id=>id}}.to_json
     post_request(url,parameters)
   end
-# 批量移动用户分组
-  def move_wx_user_list options={}
+
+  def move_wx_user_list( options={} )
     # options = {:openid_list => ["oMtqxt1LlPdU-zdC3n4DhL6mEEDU"],:to_groupid =>101}
     url = "https://api.weixin.qq.com/cgi-bin/groups/members/batchupdate?access_token=#{self.wx_access_token}"
     parameters = {:openid_list => options[:openid_list],:to_groupid => options[:to_groupid]}.to_json
     post_request(url,parameters)
   end
-# 移动用户分组
-  def move_wx_user options={}
+
+  def move_wx_user(options={})
     # options = {:openid => "oMtqxt1LlPdU-zdC3n4DhL6mEEDU",:to_groupid =>100}
     url = "https://api.weixin.qq.com/cgi-bin/groups/members/update?access_token=#{self.wx_access_token}"
     parameters = {:openid =>options[:openid],:to_groupid => options[:to_groupid]}.to_json
     post_request(url,parameters)
   end
 
-  # 查询用户所在分组
-  def get_wx_user_id openid
+  def get_wx_user_id( openid )
     #code
     url = "https://api.weixin.qq.com/cgi-bin/groups/getid?access_token=#{self.wx_access_token}"
     parameters = {:openid=> openid}.to_json
     post_request(url,parameters)["groupid"].to_i
   end
 
-  # 设置备注名
-  def set_remark options={}
-    options = {:openid => "oMtqxt1LlPdU-zdC3n4DhL6mEEDU" ,:remark => "pangzi"}
+  def set_remark( options = {} )
     url = "https://api.weixin.qq.com/cgi-bin/user/info/updateremark?access_token=#{self.wx_access_token}"
     parameters = {:openid =>options[:openid] ,:remark =>options[:remark]}.to_json
     post_request(url,parameters)
     WxUser.find_by_openid(options[:openid]).update_attributes(remark:options[:remark])
+  end
+
+  def media_upload( options = {})
+    url = "https://api.weixin.qq.com/cgi-bin/media/upload?access_token=#{self.wx_access_token}&type=#{options[:type]}"
+    parameters = {:media=>File.new("/Users/icepoint1999/Desktop/company.jpg")}
+    post_request(url,parameters)
+  end
+
+  def media_upload_news( options = {} )
+    # options={:title => "2",:thumb_media_id =>"AS_OnDp_LpWAyrON2SHkawv_3ZJoxGTkigZymwpRQ-WE9H5aSCMfFPRbkyiuMAEX",:author =>"asd",
+    # :digest =>"12",:show_cover_pic=>"1",:content=>"asdasdasd <img src='http://img.baidu.com/img/iknow/sula201601/beijingmaohou270.jpg?t=1455621428' alt=''>",:content_source_url=>"asdasd"  }
+
+    url = "https://api.weixin.qq.com/cgi-bin/media/uploadnews?access_token=#{self.wx_access_token}"
+    parameters = {:articles=>[{
+      title: options[:title],
+      thumb_media_id: options[:thumb_media_id],
+      author: options[:author],
+      digest: options[:digest],
+      show_cover_pic: options[:show_cover_pic],
+      content: options[:content],
+      content_source_url: options[:source_url]
+    }]}.to_json
+
+    post_request(url,parameters)
+
+  end
+
+  def material_add_news( options = {} )
+    url ="https://api.weixin.qq.com/cgi-bin/material/add_news?access_token=#{self.wx_access_token}"
+    params ={
+      articles: [{
+        title: options[:title],
+        thumb_media_id: options[:thumb_media_id],
+        author: options[:author],
+        digest: options[:digest],
+        show_cover_pic: options[:show_cover_pic],
+        content: options[:content],
+        content_source_url: options[:source_url]
+      }]
+    }.to_json
+
+    post_request(url,params)
+  end
+
+  def material_add_media( options = {} )
+    url = "https://api.weixin.qq.com/cgi-bin/material/add_material?access_token=#{self.wx_access_token}&type=#{options[:type]}"
+    params = {:media => File.new("#{options[:path]}")}
+
+    post_request(url,params)
+
+  end
+
+
+  def message_mass_send_all ( options = {} )
+    # options = {:media_type=>"mpnews",:is_to_all=>true ,:media_id=>"wun3zeg4dfHuVoMr2Emqzp-KtSX9lbyGMzFvzhbCY07qAaJaQ5W5XZrgK8p5E2ah"}
+    url = "https://api.weixin.qq.com/cgi-bin/message/mass/sendall?access_token=#{self.wx_access_token}"
+    if options[:media_type]!="text"
+      params = {
+        filter:{
+          is_to_all: options[:is_to_all],
+          group_id: options[:group_id]
+        },
+        options[:media_type] => {
+          :media_id => "#{options[:media_id]}"
+        },
+        msgtype:"#{options[:media_type]}"
+      }.to_json
+    else
+      params = {
+        filter:{
+          is_to_all: options[:is_to_all],
+          group_id: options[:group_id]
+        },
+        text: {
+          content: "#{options[:content]}"
+        },
+        msgtype: "text"
+      }.to_json
+
+    end
+    post_request(url,params)
   end
 
   def get_request(url)
