b65d3c63824f767473eebf2d30d7d71ab40a89aa
Mike <wenke.gd@gmail.com>
2016-02-24 15:21:29 +0800

    refactor wx

--- app/controllers/wx/messages_controller.rb
+++ app/controllers/wx/messages_controller.rb
@@ -4,10 +4,6 @@ class Wx::MessagesController <  ApplicationController
     @partialLeftNav = "/layouts/partialLeftWeixin"
   end
 
-  def index
-
-  end
-
   def send_message
     options = {:media_type=>"text",:is_to_all=>true ,:content=>params[:text_message]}
     notice=  current_site.wx_mp_user.message_mass_send_all(options)["msg_id"].nil? ? '保存失败消息发送超出上限' : '保存成功'
@@ -15,6 +11,5 @@ class Wx::MessagesController <  ApplicationController
     respond_to do |format|
       format.html { redirect_to :back, notice: notice }
     end
-    
   end
 end
