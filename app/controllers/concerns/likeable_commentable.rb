c9c40377bdde927616c89d41ac39a0a29ae1bd69
Mike <wenke.gd@gmail.com>
2016-04-19 20:28:28 +0800

    refactor LikeableCommentable

--- app/controllers/concerns/likeable_commentable.rb
+++ app/controllers/concerns/likeable_commentable.rb
@@ -1,22 +1,22 @@
 module LikeableCommentable
   def likes_comments_partial(resource, user)
+    resource.increment!(:view_count)
+
     like_params = {
-      site_id: resource.is_a?(WebsiteMenu) ? resource.website.site_id : resource.site_id,
+      site_id: (resource.is_a?(WebsiteMenu) ? resource.website.site_id : resource.site_id),
       user_id: user.try(:id),
       likeable_id: resource.id,
       likeable_type: resource.class.to_s
     }
     @like = Like.where(like_params).first
-    unless @like
-      @like = Like.new(like_params)
-    end
-    resource.increment!(:view_count)
+    @like = Like.new(like_params) unless @like
+
     comment_params = {
-      site_id: resource.is_a?(WebsiteMenu) ? resource.website.site_id : resource.site_id,
+      site_id: (resource.is_a?(WebsiteMenu) ? resource.website.site_id : resource.site_id),
       commentable_id: resource.id,
       commentable_type: resource.class.to_s,
       commenter_id: user.try(:id),
-      commenter_type: user.class.to_s
+      commenter_type: user.try(:class).to_s
     }
     @comment = Comment.new(comment_params)
     @comments = resource.comments
